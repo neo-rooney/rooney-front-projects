e1740e4b4324e94fbf779e73c1f3aca079db6f4e
rooney <bch3454@gmail.com>
2024-06-13 12:37:00 +0900

    feat: storybook 다크모드 대응

--- services/storybook/.storybook/preview.js
+++ services/storybook/.storybook/preview.js
@@ -1,5 +1,5 @@
 import "@rooney/themes/themes.css";
-
+import "./style.css";
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
@@ -9,7 +9,44 @@ const preview = {
         date: /Date$/i,
       },
     },
+    rootAttributesTooltip: true,
+    rootAttributes: [
+      {
+        root: "body",
+        attribute: "class",
+        defaultState: {
+          name: "light",
+          value: "theme-light",
+        },
+        states: [
+          {
+            name: "dark",
+            value: "theme-dark",
+          },
+        ],
+      },
+    ],
   },
 };
+
+const initTheme = () => {
+  const isDarkTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+  if (isDarkTheme) {
+    document.body.classList.add("theme-dark");
+  }
+
+  const mediaQueryList = window.matchMedia("(prefers-color-scheme: dark)");
+
+  mediaQueryList.addEventListener("change", (e) => {
+    if (e.matches) {
+      document.body.classList.add("theme-dark");
+    } else {
+      document.body.classList.remove("theme-dark");
+    }
+  });
+};
+
+initTheme();
 
 export default preview;
